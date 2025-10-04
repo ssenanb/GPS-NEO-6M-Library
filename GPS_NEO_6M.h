bfffacab7eae8700298136ecf69a7b0b20dc42c7
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2025-10-04 19:27:39 +0300

    Update GPS_NEO_6M.h

--- GPS_NEO_6M.h
+++ GPS_NEO_6M.h
@@ -28,31 +28,12 @@ typedef struct{
 	char checksum;
 }GPGGA_Data;
 
-typedef struct{
-	char utc_time[11];
-	char status;
-	double latitude;
-	char latitude_dir;
-	double longitude;
-	char longitude_dir;
-	float speed_knots;
-	float track_angle;
-	char date[7];
-	float magnetic_variation;
-	char magnetic_variation_dir;
-	char checksum;
-}GPRMC_Data;
-
 typedef enum{
 	GPGGA_SUCCESS = 1,
 	GPGGA_FAIL = 0,
 }GPGGA_Status;
 
-typedef enum{
-	GPRMC_SUCCESS = 1,
-	GPRMC_FAIL = 0,
-}GPRMC_Status;
-
 GPGGA_Status GPS_GPGGA_Data(GPGGA_Data *gpgga_data);
-//GPRMC_Status GPS_GPRMC_Data(GPRMC_Data *gprmc_data);
+
 #endif /* INC_GPS_NEO_6M_H_ */
+
